bd53700138e0915ae6d85eeb1817c7d2c20314c3
O1dMan <O1dMan@gmail.com>
2022-10-12 15:17:25 +0800

    first commit

---
+++ src/main/java/com/study/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.study.git;
+
+/**
+ * @author OldMan
+ * @Description
+ * @create 2022/10/12 - 15:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
